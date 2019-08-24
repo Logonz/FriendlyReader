abbbe2266192dfdc5aa38fe8947df12d720873e6
logon <logon.q@gmail.com>
2019-08-24 12:41:26 +0200

    Some code changes to use synpairs_emojis.xml, not finished.

--- JS/synonyms.js
+++ JS/synonyms.js
@@ -2,65 +2,95 @@
 This file contains functions regarding synonyms
 */
 
+var _GS = null;
+
 function removeSynonyms(text) {
   /*Rensar bort alla word-tooltip*/
-  $(".word-wrapper").each(function () {
+  $(".word-wrapper").each(function() {
     var tooltip = $(this).parent();
     tooltip.remove();
   });
   /*Tar bort alla spans runt ord med synonymer*/
-  $(".synonyms").contents().unwrap();
+  $(".synonyms")
+    .contents()
+    .unwrap();
   var text = $("#textarea").html();
-  return text
+  return text;
 }
 
 function updateSynonyms() {
-  let words = _GS.SynonymStore.tagText(_GS.TextStore.tokenizedText, "mouseenter", function (event) {
-    let parentSpan = $(this);
-    let intId = parseInt($(this).data("word-id"));
-    let word = $(this).data("word");
-    if (event.type === "mouseenter") {
-      // Does the child already exist? Remove it to refresh the elements.
-      if ($(this).children(".wordtooltip").length) {
-        $(this).children(".wordtooltip").remove();
-      }
-      // Create the element if it doesn't exist.
-      if (!$(this).children(".wordtooltip").length) {
-        // Create the tooltip in code and Destroy the tooltip when leaving
-        let div = $("<div>", { "class": "wordtooltip" }).on("mouseleave", function (event) {
-          $(this).remove();
-        });
-
-        // This is the click handler callback.
-        let _callback = function (event) {
-          let id = $(this).data("word-id");
-          let synonym = $(this).data("synonym");
-
-          // Actually change the word
-          _GS.TextStore.setWord(id, synonym);
-          // You can just change the data but synonyms might not exist for the new word.
-          parentSpan.attr("data-word", synonym);
-          parentSpan.text(synonym);
-          // Or you can just unwrap the span to remove all the data (Adviceable due to not having the synonyms for the new word)
-          parentSpan.contents().unwrap();
-
-          console.log("Clicked tooltip: Word-Id:", id, "Synonym:", synonym, "Event:", event, "this:", $(this));
-        };
-
-        // Get three synoynms for the word
-        let synonyms = _GS.SynonymStore.getSynonyms(word, 3);
-        // Create three elements, one for each synonym and set the text and click callback
-        for (const index in synonyms) {
-          div.append($("<div>", { "class": "word-wrapper", "data-word-id": intId, "data-synonym": synonyms[index].synonym }).text(synonyms[index].synonym).click(_callback));
+  let words = _GS.SynonymStore.tagText(
+    _GS.TextStore.tokenizedText,
+    "mouseenter",
+    function(event) {
+      let parentSpan = $(this);
+      let intId = parseInt($(this).data("word-id"));
+      let word = $(this).data("word");
+      if (event.type === "mouseenter") {
+        // Does the child already exist? Remove it to refresh the elements.
+        if ($(this).children(".wordtooltip").length) {
+          $(this)
+            .children(".wordtooltip")
+            .remove();
         }
-        // Append the wordtooltip to the synonyms span
-        $(this).append(div);
+        // Create the element if it doesn't exist.
+        if (!$(this).children(".wordtooltip").length) {
+          // Create the tooltip in code and Destroy the tooltip when leaving
+          let div = $("<div>", { class: "wordtooltip" }).on(
+            "mouseleave",
+            function(event) {
+              $(this).remove();
+            }
+          );
+
+          // This is the click handler callback.
+          let _callback = function(event) {
+            let id = $(this).data("word-id");
+            let synonym = $(this).data("synonym");
+
+            // Actually change the word
+            _GS.TextStore.setWord(id, synonym);
+            // You can just change the data but synonyms might not exist for the new word.
+            parentSpan.attr("data-word", synonym);
+            parentSpan.text(synonym);
+            // Or you can just unwrap the span to remove all the data (Adviceable due to not having the synonyms for the new word)
+            parentSpan.contents().unwrap();
+
+            console.log(
+              "Clicked tooltip: Word-Id:",
+              id,
+              "Synonym:",
+              synonym,
+              "Event:",
+              event,
+              "this:",
+              $(this)
+            );
+          };
+
+          // Get three synoynms for the word
+          let synonyms = _GS.SynonymStore.getSynonyms(word, 3);
+          // Create three elements, one for each synonym and set the text and click callback
+          for (const index in synonyms) {
+            div.append(
+              $("<div>", {
+                class: "word-wrapper",
+                "data-word-id": intId,
+                "data-synonym": synonyms[index].synonym
+              })
+                .text(synonyms[index].synonym)
+                .click(_callback)
+            );
+          }
+          // Append the wordtooltip to the synonyms span
+          $(this).append(div);
+        }
       }
     }
-  });
+  );
   return words;
 }
-var synOn = 0
+var synOn = 0;
 // This function is activated from the side menu
 function activateSynonyms() {
   var text = $("#textarea").html();
@@ -68,33 +98,89 @@ function activateSynonyms() {
   if (synOn == 0) {
     startLoading();
     _GS = Database.initialize();
-    $("#undo").click(function () {
+    $("#undo").click(function() {
       _GS.resetState();
       let words = updateSynonyms();
-      console.log("TeST", words)
+      console.log("TeST", words);
       $("#textarea").html(words.join(""));
-    })
+    });
     _GS.addStore("TextStore", new TextStore(text));
     _GS.addStore("SynonymStore", new SynonymStore());
 
-    let resp = _GS.SynonymStore.analyzeText(text, function (data) {
+    let resp = _GS.SynonymStore.analyzeText(text, function(data) {
       if (data) {
         let words = updateSynonyms();
-        console.log("TeST", words)
+        console.log("TeST", words);
         $("#textarea").html(words.join(""));
         endLoading();
-      }
-      else {
+      } else {
+        console.log("Error analyzing text");
+        showNegativeFeedback("Någonting gick fel");
+        var localSynonyms = loadSynonymsXML();
+        console.log(localSynonyms);
+        _GS.SynonymStore.data = localSynonyms;
+        let words = updateSynonyms();
+        console.log("TeST", words);
+        $("#textarea").html(words.join(""));
+        endLoading();
         //FEL!
       }
     });
-    synOn = 1
+    synOn = 1;
     sidenavControl("none");
-
   } else {
     newtext = removeSynonyms(text);
     $("#textarea").html(newtext);
-    synOn = 0
+    synOn = 0;
     sidenavControl("none");
   }
-};
+}
+
+function loadSynonymsXML() {
+  /*
+    Öppnar XML-fil och returnerar innehållet i ett objekt.
+  */
+  var xmlDict = [];
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4) {
+      var xml = xmlhttp.responseXML;
+      var synonyms = xml.getElementsByTagName("syn");
+
+      for (var i = 0; i < synonyms.length; i++) {
+        // Gör variabler för att återanvända dem i kortare kod nedan
+        var _w1 = synonyms[i].getElementsByTagName("w1")[0].childNodes[0];
+        var _w2 = synonyms[i].getElementsByTagName("w2")[0].childNodes[0];
+
+        // Kollar att alla ord i lexikonet har synonymer till sig
+        if (_w1 === undefined || _w2 === undefined) {
+          console.log("saknas text i:", _w1, _w2);
+        }
+        // Gör variabler för att korta ner koden nedan
+        var w1 = _w1.nodeValue;
+        var w2 = _w2.nodeValue;
+        var level = synonyms[i].getAttribute("level");
+
+        // Synonymerna läggs in i ett dictionary
+        if (w1 in xmlDict) {
+          //xmlDict[w1].push([level, w2]);
+          //console.log(xmlDict[w1]);
+          if (Array.isArray(xmlDict[w1]) == false) {
+            xmlDict[w1] = [];
+          }
+          xmlDict[w1].push({ level: parseFloat(level), synonym: w2 });
+        } else {
+          xmlDict[w1] = [];
+          xmlDict[w1].push({ level: parseFloat(level), synonym: w2 });
+        }
+        //xmlDict[w1].push({"level": parseFloat(level), "synonym": w2});
+      }
+      console.log("CLIENT.Synonyms: Inläsning klar!");
+    }
+  };
+  xmlhttp.open("GET", "synpairs_emojis.xml", false);
+  xmlhttp.send();
+  console.log("CLIENT.Synonyms: Läser in synonymlexikon...");
+  return xmlDict;
+}
